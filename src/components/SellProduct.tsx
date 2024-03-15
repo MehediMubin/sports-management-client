ca409ddd009c57b848bb070756fabb5bc72a0bf5
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-03-15 20:35:40 +0600

    invoice download option implemented

--- src/components/SellProduct.tsx
+++ src/components/SellProduct.tsx
@@ -1,9 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+   Document,
+   PDFDownloadLink,
+   Page,
+   StyleSheet,
+   Text,
+   View,
+} from "@react-pdf/renderer";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Modal from "react-modal";
 import { toast } from "sonner";
 import { useGetProductByIdQuery } from "../redux/features/product/productApi";
 import { useCreateSaleMutation } from "../redux/features/sales/salesApi";
+
+const styles = StyleSheet.create({
+   page: {
+      padding: 30,
+   },
+   section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+   },
+});
+
+Modal.setAppElement("#root");
+
+const MyDocument = ({ sellingInfo }) => (
+   <Document>
+      <Page size="A4" style={styles.page}>
+         <View style={styles.section}>
+            <Text>Product Name: {sellingInfo?.productName}</Text>
+            <Text>Buyer Name: {sellingInfo?.buyerName}</Text>
+            <Text>Quantity: {sellingInfo?.quantity}</Text>
+            <Text>Date: {sellingInfo?.date}</Text>
+         </View>
+      </Page>
+   </Document>
+);
 
 const SellProduct = ({ productId }: { productId: any }) => {
    const [isFormOpen, setIsFormOpen] = useState(false);
@@ -11,19 +46,50 @@ const SellProduct = ({ productId }: { productId: any }) => {
    const [sellProduct] = useCreateSaleMutation();
    const { register, handleSubmit, reset } = useForm();
    const { data: product, isLoading } = useGetProductByIdQuery(productId);
+   const [sellingInfo, setSellingInfo] = useState<any>(null);
+
+   const [modalIsOpen, setIsOpen] = useState(false);
+   function openModal() {
+      setIsOpen(true);
+   }
+
+   function closeModal() {
+      setIsOpen(false);
+   }
+
+   const customStyles = {
+      content: {
+         top: "50%",
+         left: "50%",
+         right: "auto",
+         bottom: "auto",
+         marginRight: "-50%",
+         transform: "translate(-50%, -50%)",
+         backgroundColor: "#282c34",
+         border: "1px solid #888",
+         padding: "20px",
+         color: "#fff",
+         borderRadius: "10px",
+         boxShadow: "0px 0px 15px 5px rgba(0, 0, 0, 0.2)",
+         textAlign: "center",
+      },
+   };
 
    const onSubmit = async (data: FieldValues) => {
-      const sellingInfo = {
+      const newSellingInfo = {
          productId,
+         productName: product.data.name,
          buyerName: data.buyerName,
          quantity: data.quantity,
          branch: data.branch,
          date: data.date,
       };
 
+      setSellingInfo(newSellingInfo);
+      openModal();
       setIsFormOpen(false);
       reset();
-      const res = await sellProduct(sellingInfo);
+      const res = await sellProduct(newSellingInfo);
 
       if ("error" in res && res.error) {
          toast.error("Something went wrong! Please try again later.", {
@@ -40,7 +106,7 @@ const SellProduct = ({ productId }: { productId: any }) => {
    if (isLoading) return <div>Loading...</div>;
 
    return (
-      <div>
+      <div id="root">
          <button
             className="btn btn-primary"
             onClick={() => setIsFormOpen(true)}
@@ -146,6 +212,22 @@ const SellProduct = ({ productId }: { productId: any }) => {
                </div>
             </div>
          )}
+         <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+         >
+            <h2>Product Sold</h2>
+            <PDFDownloadLink
+               document={<MyDocument sellingInfo={sellingInfo} />}
+               fileName="invoice.pdf"
+            >
+               {({ blob, url, loading, error }) =>
+                  loading ? "Loading document..." : "Download Invoice"
+               }
+            </PDFDownloadLink>
+         </Modal>
       </div>
    );
 };
