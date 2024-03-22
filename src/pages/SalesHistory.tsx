72b73dfe66b1b59f6f17bf72c4952fb0eff0bf36
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-03-22 15:20:24 +0600

    download sales report implemented

--- src/pages/SalesHistory.tsx
+++ src/pages/SalesHistory.tsx
@@ -1,3 +1,11 @@
+import {
+   Document,
+   PDFDownloadLink,
+   Page,
+   StyleSheet,
+   Text,
+   View,
+} from "@react-pdf/renderer";
 import { useEffect, useState } from "react";
 import FilterBranch from "../components/FilterBranch";
 import FilterSales from "../components/FilterSales";
@@ -13,24 +21,42 @@ import { useGetBranchQuery } from "../redux/features/user/userApi";
 import { useAppSelector } from "../redux/hooks";
 import { RootState } from "../redux/store";
 
+const styles = StyleSheet.create({
+   page: {
+      padding: 30,
+   },
+   section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+   },
+});
+
+const MyDocument = ({ salesReport }) => (
+   <Document>
+      <Page size="A4" style={styles.page}>
+         <View style={styles.section}>
+            <Text>Total Expense: {salesReport?.totalSellAmount}</Text>
+            <Text>Total Sales: {salesReport?.totalExpense}</Text>
+            <Text>Profit: {salesReport?.profit}</Text>
+            <Text>Loss: {salesReport?.loss}</Text>
+         </View>
+      </Page>
+   </Document>
+);
+
 const SalesHistory = () => {
    const [filterSales, setFilterSales] = useState("all-time");
    const [filter, setFilter] = useState("All Branches");
+   const [salesReport, setSalesReport] = useState({});
 
    const user = useAppSelector((state: RootState) => state.auth.user);
    const { role, username } = user;
    const { data: branchData, isLoading: branchNameLoading } =
       useGetBranchQuery(username);
 
-   useEffect(() => {
-      if (filter === "All Branches") {
-         setFilter("all-branches");
-      }
-
-      if (role === "branchManager") {
-         setFilter(branchData?.data || "all-branches");
-      }
-   }, [filter, role, branchData]);
+   let totalQuantity = 0,
+      totalSellAmount = 0;
 
    const { data, isLoading, isSuccess } = useGetSalesHistoryAllTimeQuery({
       branchName: filter,
@@ -61,9 +87,6 @@ const SalesHistory = () => {
    } = useGetSalesHistoryThisYearQuery({ branchName: filter });
 
    if (isLoading) return <div>Loading...</div>;
-
-   let totalQuantity = 0,
-      totalSellAmount = 0;
 
    switch (filterSales) {
       case "today":
@@ -104,6 +127,27 @@ const SalesHistory = () => {
          break;
    }
 
+   useEffect(() => {
+      if (filter === "All Branches") {
+         setFilter("all-branches");
+      }
+
+      if (role === "branchManager") {
+         setFilter(branchData?.data || "all-branches");
+      }
+
+      const totalExpense = Math.round(totalSellAmount * 0.8);
+      const profit = totalSellAmount - totalExpense;
+      const loss = 0;
+      const newSalesReport = {
+         totalExpense,
+         totalSellAmount,
+         profit,
+         loss,
+      };
+      setSalesReport(newSalesReport);
+   }, [filter, role, branchData, totalSellAmount]);
+
    return (
       <div className="w-8/12 mx-auto mt-3">
          {role === "seller" ? (
@@ -128,9 +172,16 @@ const SalesHistory = () => {
                   </div>
 
                   <div>
-                     <button className="btn btn-warning mt-3">
-                        Download Sales Report
-                     </button>
+                     <PDFDownloadLink
+                        document={<MyDocument salesReport={salesReport} />}
+                        fileName="sales-report.pdf"
+                     >
+                        {({ blob, url, loading, error }) =>
+                           loading
+                              ? "Loading document..."
+                              : "Download Sales Report"
+                        }
+                     </PDFDownloadLink>
                   </div>
                </div>
                <div className="flex justify-between gap-2 lg:gap-0">
