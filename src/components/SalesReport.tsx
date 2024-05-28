4dc53620a37fa44a94a13de3d8c2fc66c4c5dda3
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-05-28 10:59:29 +0600

    modified sales report

--- src/components/SalesReport.tsx
+++ src/components/SalesReport.tsx
@@ -2,12 +2,41 @@ import { Document, Page, StyleSheet, Text, View } from "@react-pdf/renderer";
 
 const styles = StyleSheet.create({
    page: {
+      flexDirection: "column",
+      backgroundColor: "#ffffff",
       padding: 30,
    },
    section: {
       margin: 10,
       padding: 10,
       flexGrow: 1,
+   },
+   title: {
+      fontSize: 30,
+      textAlign: "center",
+      marginBottom: 20,
+      fontWeight: "bold",
+   },
+   item: {
+      flexDirection: "row",
+      marginBottom: 15,
+      borderBottomColor: "#000",
+      borderBottomWidth: 1,
+      alignItems: "stretch",
+   },
+   itemColumn: {
+      flexDirection: "column",
+      margin: 10,
+   },
+   itemName: {
+      fontSize: 20,
+      fontWeight: "bold",
+   },
+   itemQuantity: {
+      fontSize: 18,
+   },
+   itemDate: {
+      fontSize: 18,
    },
 });
 
@@ -16,8 +45,35 @@ const SalesReport = ({ reportData }) => (
    <Document>
       <Page size="A4" style={styles.page}>
          <View style={styles.section}>
-            <Text>Total Quantity: {reportData.totalQuantity}</Text>
-            <Text>Total Sell Amount: {reportData.totalSellAmount}</Text>
+            <Text style={styles.title}>Sales Report</Text>
+            <View style={styles.item}>
+               <View style={styles.itemColumn}>
+                  <Text style={styles.itemName}>Total Quantity:</Text>
+                  <Text style={styles.itemQuantity}>
+                     {reportData.totalQuantity}
+                  </Text>
+               </View>
+               <View style={styles.itemColumn}>
+                  <Text style={styles.itemName}>Total Sell Amount:</Text>
+                  <Text style={styles.itemQuantity}>
+                     {reportData.totalSellAmount}
+                  </Text>
+               </View>
+               <View style={styles.itemColumn}>
+                  <Text style={styles.itemName}>Total Expense:</Text>
+                  <Text style={styles.itemQuantity}>
+                     {reportData.totalExpense}
+                  </Text>
+               </View>
+               <View style={styles.itemColumn}>
+                  <Text style={styles.itemName}>Profit:</Text>
+                  <Text style={styles.itemQuantity}>{reportData.profit}</Text>
+               </View>
+               <View style={styles.itemColumn}>
+                  <Text style={styles.itemName}>Loss:</Text>
+                  <Text style={styles.itemQuantity}>{reportData.loss}</Text>
+               </View>
+            </View>
          </View>
       </Page>
    </Document>
